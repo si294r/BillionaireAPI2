63cebb84e92f66f588b3e1f196e5b407e1bee727
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-04-07 09:49:26 +0700

    Initial Commit

--- get_event.php
+++ get_event.php
@@ -21,20 +21,10 @@ $config['hostname'] = 'mongodb_server';
 $config['database'] = 'billionaire_prod';
 $config['database_dev'] = 'billionaire_dev';
 
-//$db_host = 'localhost';
-//$db_user = 'root';
-//$db_pass = 'password';
-//$db_name = 'Billionaire_prod';
-//$tbl_event = 'event';
-
 $json['current_time'] = gmdate('Y-m-d H:i:s');
 $json['device'] = $device;
 $json['version'] = $version;
 $json['exception'] = "";
-
-//if ($debug == "1") {
-//    $tbl_event = str_replace("_prod", "_dev", $db_name) . "." . $tbl_event;
-//}
 
 try {
     
@@ -73,28 +63,10 @@ try {
         $json['event_time']['end'] = $array['end_date'];
     }
     
-//    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-//    $query = "select start_date, end_date from " . $tbl_event . " "
-//            . "where `device` = ? and `version` = ? and `status`='active' ";
-
-//    if ($stmt = $mysqli->prepare($query)) {
-//
-//        $stmt->bind_param("ss", $device, $version);
-//        $stmt->execute();
-//        $stmt->bind_result($start_date, $end_date);
-//
-//        if ($stmt->fetch()) {
-//            $json['event_time']['start'] = $start_date;
-//            $json['event_time']['end'] = $end_date;
-//        }
-//
-//        $stmt->close();
-//    }
 } catch (Exception $ex) {
     $json['exception'] = $ex->getMessage();
 } finally {
-//    $mysqli->close();
+
 }
 
 $end_time = microtime(true);
