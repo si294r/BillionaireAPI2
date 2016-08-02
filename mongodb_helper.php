113c2368106ea8d3513b4bc4a5d27199672e2295
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-08-02 16:45:45 +0700

    Update Commit

--- mongodb_helper.php
+++ mongodb_helper.php
@@ -30,11 +30,14 @@ function bson_oid($id) {
 }
 
 // convert BSON Object Document to Array PHP
-function bson_document_to_array($document) {
+function bson_document_to_array($document, $fields = []) {
     $array = NULL;
 
     if (is_object($document)) {
         foreach ($document as $k => $v) {
+            if (count($fields) > 0 && !in_array($k, $fields)) {
+                continue;
+            }
             if (is_object($v) && "MongoDB\Model\BSONDocument" == get_class($v)) {
                 $array[$k] = bson_document_to_array($v);
             } elseif (is_object($v)) {
@@ -48,12 +51,12 @@ function bson_document_to_array($document) {
 }
 
 // convert BSON Object Documents to Array PHP
-function bson_documents_to_array($documents) {
+function bson_documents_to_array($documents, $fields = []) {
     $array = array();
 
     if (is_object($documents)) {
         foreach ($documents as $document) {
-            $array[] = bson_document_to_array($document);
+            $array[] = bson_document_to_array($document, $fields);
         }
     }
     return $array;
