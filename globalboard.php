c2dd585e660e66832aea398b624cb9c52bbd7bc8
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-08-02 15:26:01 +0700

    Update Commit

---
+++ globalboard.php
@@ -0,0 +1,68 @@
+<?php
+
+defined('IS_DEVELOPMENT') OR exit('No direct script access allowed');
+
+require 'mongodb_helper.php';
+
+$facebook_id = isset($params[1]) ? $params[1] : "";
+$limit = isset($params[2]) ? $params[2] : 50;
+        
+if (trim($facebook_id) == "") {
+    return array(
+        "code" => 141,
+        "error" => "parameter facebook id not found"
+    );
+}
+
+$db = get_mongodb(IS_DEVELOPMENT);
+$collection = $db->selectCollection("_User");
+
+$document = $collection->findOne([ 'facebook_id' => $facebook_id ]);
+
+if (!is_object($document)) {
+    return array("code" => 141, "error" => "User not found");
+}
+
+$filter = array();
+$sort = array('netWorth_pow' => -1, 'netWorth_2' => -1, 'facebook_id' => -1); // desc(-1), asc(1)
+$options = array('sort' => $sort, 'limit' => (int) $limit);
+
+$documents = $collection->find($filter, $options);
+
+$result['status'] = TRUE;
+$result['currentUser'] = bson_document_to_array($document);
+$result['topPlayer'] = bson_documents_to_array($documents);
+
+//$score = isset($document->score) ? $document->score : 0;
+$count1 = 0; //$collection->count(array('score' => array('$gt' => $score)));
+$count2 = 0; //$collection->count(array('score' => array('$eq' => $score), 'facebook_id' => array('$gte' => $facebook_id)));
+
+$i = 1;
+$facebook_ids = array($facebook_id);
+foreach ($result['topPlayer'] as $k=>$v) {
+//    $result['topPlayer'][$k]['name'] = 'Player '.$i;
+    if (trim($v['facebook_id']) != "") {
+        $facebook_ids[] = $v['facebook_id'];
+    }
+    $result['topPlayer'][$k]['rank'] = $i;
+    $i++;
+}
+
+$url = "https://graph.facebook.com/?ids=" . implode(",", $facebook_ids) . "&access_token=" . $config['facebook_token'];
+$result_facebook = file_get_contents($url);
+$json_facebook = json_decode($result_facebook);
+
+$result['currentUser']['name'] = isset($json_facebook->$facebook_id->name) ? $json_facebook->$facebook_id->name : "N/A";
+$result['currentUser']['rank'] = $count1 + $count2;
+
+foreach ($result['topPlayer'] as $k=>$v) {
+    if (trim($v['facebook_id']) != "" && isset($json_facebook->$v['facebook_id']->name)) {
+        $result['topPlayer'][$k]['name'] = $json_facebook->$v['facebook_id']->name;
+    } else {
+        $result['topPlayer'][$k]['name'] = "N/A";
+    }
+}
+
+//echo json_encode($result);
+
+return $result;
---
+++ leaderboard.php
@@ -0,0 +1,48 @@
+<?php
+
+$start_time = microtime(true);
+
+define('IS_DEVELOPMENT', true);
+define('BILLIONAIRE_TOKEN', '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+function show_error($response_code, $status_code, $message) {
+    http_response_code($response_code);
+    header('Content-Type: application/json');
+    echo json_encode(array('status_code' => $status_code, 'message' => $message));
+    die;
+}
+
+$headers = getallheaders();
+if (!isset($headers['Billionaire-Token']) || $headers['Billionaire-Token'] != JELLY_POP_TOKEN) {
+    show_error(401, "401 Unauthorized", "Invalid Billionaire Token");
+}
+
+$query_string = isset($_SERVER["QUERY_STRING"]) ? $_SERVER["QUERY_STRING"] : "";
+$params = explode("/", $query_string);
+
+$service = isset($params[0]) ? $params[0] : "";
+
+switch ($service) {
+    case 'globalboard' :
+    case 'localboard' :
+        if ($_SERVER["REQUEST_METHOD"] != 'GET') {
+            show_error(405, "405 Method Not Allowed", "Invalid Method");
+        }
+        try {
+            $service_result = include($service.'.php');
+        } catch (Exception $ex) {
+            show_error(500, "500 Internal Server Error", $ex->getMessage());
+        }
+        break;
+    default :
+        show_error(503, "503 Service Unavailable", "Invalid Service");
+}
+
+$end_time = microtime(true);
+
+$service_result['execution_time'] = number_format($end_time - $start_time, 5);
+$service_result['memory_usage'] = memory_get_usage(true);
+
+header('Content-Type: application/json');
+
+echo json_encode($service_result);
