dbf30fbcb46d43842b8a504c20d4d90faf0e4544
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-08-03 14:24:33 +0700

    Update Commit

--- globalboard.php
+++ globalboard.php
@@ -6,9 +6,9 @@ require 'mongodb_helper.php';
 
 $facebookID = isset($params[1]) ? $params[1] : "";
 $limit = isset($params[2]) ? $params[2] : 50;
-$overwrite_cache = isset($params[3]) ? $params[3] : 0;
+$overwrite_top_player_cache = isset($params[3]) ? $params[3] : 0;
 
-if (trim($facebookID) == "") {
+if (trim($facebookID) == "" && $overwrite_top_player_cache == 0) {
     return array(
         "code" => 141,
         "error" => "parameter facebook id not found"
@@ -20,7 +20,7 @@ $collection = $db->selectCollection("_User");
 
 $fields = ['facebookID', 'netWorth', 'netWorth_2', 'netWorth_pow', 'displayName'];
 
-if ($overwrite_cache == 0) {
+if ($overwrite_top_player_cache == 0) {
     $document = $collection->findOne([ 'facebookID' => $facebookID]);
 
     if (!is_object($document)) {
@@ -30,9 +30,10 @@ if ($overwrite_cache == 0) {
     $result['currentUser'] = bson_document_to_array($document, $fields);
 }
 
+$update_top_player_cache = 0;
 $key = "BillionaireAPI/leaderboard.php?globalboard";
 $array_cache = apcu_fetch($key);
-if ($array_cache === FALSE || $overwrite_cache == 1) {
+if ($array_cache === FALSE || $overwrite_top_player_cache == 1) {
     $filter = array();
     $sort = array('netWorth_pow' => -1, 'netWorth_2' => -1, 'facebookID' => -1); // desc(-1), asc(1)
     $options = array('sort' => $sort, 'limit' => (int) $limit);
@@ -40,11 +41,11 @@ if ($array_cache === FALSE || $overwrite_cache == 1) {
     $documents = $collection->find($filter, $options);
 
     $array_cache = bson_documents_to_array($documents, $fields);
-    apcu_store($key, $array_cache, 240);
+    $update_top_player_cache = 1;
 }
 $result['topPlayer'] = $array_cache;
 
-if ($overwrite_cache == 0) {
+if ($overwrite_top_player_cache == 0) {
     $netWorth_pow = isset($document->netWorth_pow) ? $document->netWorth_pow : 0;
     $netWorth_2 = isset($document->netWorth_2) ? $document->netWorth_2 : 0;
     $count1 = $collection->count(array('netWorth_pow' => array('$gt' => $netWorth_pow)));
@@ -61,31 +62,37 @@ if ($overwrite_cache == 0) {
     $facebook_ids = array();
 }
 
-$i = 1;
-foreach ($result['topPlayer'] as $k => $v) {
+if ($update_top_player_cache == 1) {
+    $i = 1;
+    foreach ($result['topPlayer'] as $k => $v) {
 //    $result['topPlayer'][$k]['name'] = 'Player '.$i;
-    if (trim($v['facebookID']) != "") {
-        $facebook_ids[] = $v['facebookID'];
+        if (trim($v['facebookID']) != "") {
+            $facebook_ids[] = $v['facebookID'];
+        }
+        $result['topPlayer'][$k]['rank'] = $i;
+        $i++;
     }
-    $result['topPlayer'][$k]['rank'] = $i;
-    $i++;
 }
 
 $url = "https://graph.facebook.com/?ids=" . implode(",", $facebook_ids) . "&access_token=" . $config['facebook_token'];
 $result_facebook = file_get_contents($url);
 $json_facebook = json_decode($result_facebook);
 
-if ($overwrite_cache == 0) {
+if ($overwrite_top_player_cache == 0) {
     $result['currentUser']['name'] = isset($json_facebook->$facebookID->name) ? $json_facebook->$facebookID->name : "N/A";
     $result['currentUser']['rank'] = $count1 + $count2 + $count3;
 }
 
-foreach ($result['topPlayer'] as $k => $v) {
-    if (trim($v['facebookID']) != "" && isset($json_facebook->$v['facebookID']->name)) {
-        $result['topPlayer'][$k]['name'] = $json_facebook->$v['facebookID']->name;
-    } else {
-        $result['topPlayer'][$k]['name'] = "N/A";
+if ($update_top_player_cache == 1) {
+    foreach ($result['topPlayer'] as $k => $v) {
+        if (trim($v['facebookID']) != "" && isset($json_facebook->$v['facebookID']->name)) {
+            $result['topPlayer'][$k]['name'] = $json_facebook->$v['facebookID']->name;
+        } else {
+            $result['topPlayer'][$k]['name'] = "N/A";
+        }
     }
+
+    apcu_store($key, $result['topPlayer'], 240);
 }
 
 $result['status'] = TRUE;
