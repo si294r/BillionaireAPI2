0741810233741496cdd9b0e6bf406587b375f2d4
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-08-08 14:18:19 +0700

    Update Commit

--- mongodb_helper.php
+++ mongodb_helper.php
@@ -5,26 +5,27 @@ defined('IS_DEVELOPMENT') OR exit('No direct script access allowed');
 require '/var/www/vendor/autoload.php';
 require '/var/www/mongodb_billionaire.php';
 
-
 // get mongodb object database
 function get_mongodb($is_development = false, $use_balancer = true) {
 
     global $config;
 
-    $value = apcu_fetch("BillionaireAPI_Balancer_DB");
-    if ($value === FALSE) {
-        $balancer = 1;
-    } else {
-        $balancer = $value + 1;
-        if ($balancer > count($config['hostname_balancer'])) {
+    if ($use_balancer) {
+        $value = apcu_fetch("BillionaireAPI_Balancer_DB");
+        if ($value === FALSE) {
             $balancer = 1;
+        } else {
+            $balancer = $value + 1;
+            if ($balancer > count($config['hostname_balancer'])) {
+                $balancer = 1;
+            }
         }
+        apcu_store("BillionaireAPI_Balancer_DB", $balancer);
     }
-    apcu_store("BillionaireAPI_Balancer_DB", $balancer);
-    
+
     $database = $is_development == true ? $config['database_dev'] : $config['database'];
     $hostname = $use_balancer == true ? $config['hostname_balancer'][$balancer - 1] : $config['hostname'];
-            
+
     $connection_string = "mongodb://"
             . $config['username'] . ":"
             . $config['password'] . "@"
